07b866c30492f1a90389ad32a137bafc96fe7891
monsieurverde <domverdemgmt@gmail.com>
2020-11-20 01:53:15 -0500

    new stuff

---
+++ precache-manifest.4837964d1d1dddf4fd623990d67dd149.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "481599a861914d605016e45bb23bf13f",
+    "url": "/react-vfx/index.html"
+  },
+  {
+    "revision": "0bc153ea02898d1a97ec",
+    "url": "/react-vfx/static/css/2.53e65fd0.chunk.css"
+  },
+  {
+    "revision": "b8f4538aef601a929db9",
+    "url": "/react-vfx/static/css/main.e9a2e8a9.chunk.css"
+  },
+  {
+    "revision": "0bc153ea02898d1a97ec",
+    "url": "/react-vfx/static/js/2.ef196280.chunk.js"
+  },
+  {
+    "revision": "9d3a2d98b698f760103d62357ad1d9e9",
+    "url": "/react-vfx/static/js/2.ef196280.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b8f4538aef601a929db9",
+    "url": "/react-vfx/static/js/main.8f25574a.chunk.js"
+  },
+  {
+    "revision": "717b0a951c92951e26a7",
+    "url": "/react-vfx/static/js/runtime-main.73b26e45.js"
+  }
+]);
